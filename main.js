ee1eabe847cb941ca2784cec1e34b60e008c1277
ifaamrillah <amrillah.work@gmail.com>
2024-12-23 15:06:58 +0700

    feat: install morgan & add logger

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+
+const logger = morgan(
+  `:method :url :status content_length=:res[content-length] content_type=:res[content-type] - :response-time ms`
+);
+
 const taskRouter = require("./TaskRouter");
 
 const app = express();
 app.use(express.json());
+app.use(logger);
 
 app.use("/api/v1/tasks", taskRouter);
 
