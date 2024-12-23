a1a9b1fc2e091ae31ac0f7934b685a202a5b1467
ifaamrillah <amrillah.work@gmail.com>
2024-12-23 14:08:48 +0700

    feat: install express & simple crud

---
+++ TaskController.js
@@ -0,0 +1,104 @@
+const { randomUUID } = require("crypto");
+const { STATUS_CODES } = require("http");
+
+class TaskController {
+  #tasks = [
+    {
+      id: randomUUID(),
+      title: "Task 1",
+      status: "todo",
+    },
+    {
+      id: randomUUID(),
+      title: "Task 2",
+      status: "todo",
+    },
+    {
+      id: randomUUID(),
+      title: "Task 3",
+      status: "onprogress",
+    },
+  ];
+
+  getAll = (req, res) => {
+    res.status(200).json(this.#tasks);
+  };
+
+  create = (req, res) => {
+    const { title } = req.body;
+
+    if (title === undefined || title.length < 3) {
+      res.status(400).json({
+        status: STATUS_CODES[400],
+        message: `title is required and must greater than 3 characters`,
+      });
+      return;
+    }
+
+    const newTask = {
+      id: randomUUID(),
+      title,
+      status: "todo",
+    };
+    this.#tasks.push(newTask);
+    res.status(201).json(newTask);
+  };
+
+  update = (req, res) => {
+    const { id, status } = req.body;
+
+    if (id === undefined) {
+      res.status(400).json({
+        status: STATUS_CODES[400],
+        message: `id is required`,
+      });
+      return;
+    }
+
+    if (status === undefined) {
+      res.status(400).json({
+        status: STATUS_CODES[400],
+        message: `status is required`,
+      });
+      return;
+    }
+
+    const index = this.#tasks.findIndex((task) => task.id === id);
+    if (index < 0) {
+      res.status(400).json({
+        status: STATUS_CODES[400],
+        message: `task not found`,
+      });
+      return;
+    }
+
+    this.#tasks[index].status = status;
+    res.status(200).json(this.#tasks[index]);
+  };
+
+  remove = (req, res) => {
+    const { id } = req.body;
+
+    if (id === undefined) {
+      res.status(400).json({
+        status: STATUS_CODES[400],
+        message: `id is required`,
+      });
+      return;
+    }
+
+    const deletedTask = this.#tasks.find((task) => task.id === id);
+    if (deletedTask === undefined) {
+      res.json.status(400).json({
+        status: STATUS_CODES[400],
+        message: `task not found`,
+      });
+      return;
+    }
+
+    this.#tasks = this.#tasks.filter((task) => task.id !== deletedTask.id);
+    res.status(200).json(deletedTask);
+  };
+}
+
+module.exports = TaskController;
--- main.js
+++ main.js
@@ -1,90 +1,18 @@
-const { createServer } = require("node:http");
+const express = require("express");
+const TaskController = require("./taskController");
 
-const server = createServer();
+const app = express();
 
-const htmlHomepage = `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
-      <body>
-        <h1>Homepage</h1>
-      </body>
-    </html>
-    `;
+const taskController = new TaskController();
 
-const htmlAbout = `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
-      <body>
-        <h1>About</h1>
-      </body>
-    </html>
-    `;
+app.use(express.json());
 
-const htmlNotFound = `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
-      <body>
-        <h1>404 - Not Found</h1>
-      </body>
-    </html>
-    `;
+app.get("/api/v1/tasks", taskController.getAll);
+app.post("/api/v1/tasks", taskController.create);
+app.put("/api/v1/tasks", taskController.update);
+app.delete("/api/v1/tasks", taskController.remove);
 
-server.on("request", (req, res) => {
-  const { method, url, headers } = req;
-
-  console.debug("request recieved :", {
-    method,
-    url,
-    headers: { "content-type": headers["content-type"] },
-  });
-
-  switch (url) {
-    case "/home":
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-        "content-length": htmlHomepage.length,
-      });
-      res.end(htmlHomepage);
-      return;
-    case "/about":
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-        "content-length": htmlAbout.length,
-      });
-      res.end(htmlAbout);
-      return;
-    default:
-      res.writeHead(404, {
-        "Content-Type": "text/html",
-        "content-length": htmlNotFound.length,
-      });
-      res.end(htmlNotFound);
-      return;
-  }
-});
-
-server.on("connection", (socket) => {
-  const remoteAddress = socket.remoteAddress;
-  const remotePort = socket.remotePort;
-  console.log(`New connection from ${remoteAddress} using port ${remotePort}`);
-});
-
-const port = 8000;
-server.listen(port, "localhost", () => {
-  console.log(`Server listening on port ${port}`);
+const port = process.env.PORT ?? 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
